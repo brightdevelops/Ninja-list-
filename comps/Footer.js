1bcee9643b13a754d20803234e9ffef299ee4744
brightdevelops <141080929+brightdevelops@users.noreply.github.com>
2025-01-16 11:41:36 +0100

    Update Footer.js

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer>Copyright 2025 <a href="/">Bright Develops</a></footer>;
+  return <footer>Copyright 2025 <a href="www.facebook.com">Bright Develops</a></footer>;
 };
 
 export default Footer;
